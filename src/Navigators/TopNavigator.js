637b26156a9bcc960dade23f71275c8f19e0e70f
nazimudheen267 <nazimudheen267@gmail.com>
2022-12-06 18:39:25 +0530

    challenge completed

--- src/Navigators/TopNavigator.js
+++ src/Navigators/TopNavigator.js
@@ -1,5 +1,5 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import Icon from "react-native-vector-icons/AntDesign"
+import Icon from "react-native-vector-icons/FontAwesome"
 import Videos from "../Screens/Videos";
 import Chapter from "../Screens/Chapter";
 import Resources from "../Screens/Resources";
@@ -7,17 +7,32 @@ import Qn from "../Screens/Qn";
 
 const Tab = createMaterialTopTabNavigator();
 
-export default function TopTab () {
+export default function TopTab() {
     return (
         <Tab.Navigator screenOptions={{
-            tabBarLabel: {
-            
+            tabBarPressColor: "green",
+            tabBarLabelStyle: { fontSize: 12 },
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "#555",
+            tabBarIndicatorStyle: {
+                backgroundColor: "red"
+            },
+            tabBarLabelStyle: {
+                textTransform: 'none'
             }
         }}>
-            <Tab.Screen name="Videos" component={Videos} options={({ route }) => ({title: route.name})}/>
-            <Tab.Screen name="Chapter" component={Chapter} />
-            <Tab.Screen name="Resources" component={Resources} />
-            <Tab.Screen name="Qn" component={Qn} />
+            <Tab.Screen name="Videos" component={Videos} options={{
+                tabBarIcon: ({ focused }) => <Icon name="video-camera" color={focused ? "red" : "#555"} size={19}/>
+            }}/>
+            <Tab.Screen name="Chapter" component={Chapter} options={{
+                tabBarIcon: ({ focused }) => <Icon name="book" color={focused ? "red" : "#555"} size={19} />
+            }} />
+            <Tab.Screen name="Resources" component={Resources} options={{
+                tabBarIcon: ({ focused }) => <Icon name="sitemap" color={focused ? "red" : "#555"} size={19} />
+            }} />
+            <Tab.Screen name="Qn Bank" component={Qn} options={{
+                tabBarIcon: ({ focused }) => <Icon name="question-circle" color={focused ? "red" : "#555"} size={19} />
+            }} />
         </Tab.Navigator>
     )
 }
